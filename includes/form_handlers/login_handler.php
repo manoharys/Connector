0ab666f056158107cf28d8e4f5b91fa25a8a73b5
manohar <manumanohar1527@gmail.com>
2020-07-06 10:38:00 +0530

    reopening a closed account

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -8,13 +8,20 @@
 
      //check database
 
-     $check_database_query = mysqli_query($con, "SELECT * FROM users WHERE email = '$email' and password='$password';");
+     $check_database_query = mysqli_query($con, "SELECT * FROM users WHERE email = '$email' and password='$password'");
 
      $is_exists = mysqli_num_rows($check_database_query);
 
      if($is_exists == 1){
          $rows = mysqli_fetch_array($check_database_query);
          $username = rows['username'];
+    
+        //reopening user account when login
+         $userr_closed_query = mysqli_query($con, "SELECT * FROM users WHERE email = '$email' AND user_closed = 'yes'");
+
+         if(mysqli_num_rows($userr_closed_query) == 1){
+            $reopen_account = mysqli_query($con, "UPDATE users SET user_closed = 'no' WHERE email = '$email'");
+         }
 
          $_SESSION['username'] = $username; //Storing username in session variable 
          header("Location: index.php");
