9fb28fef318ea3aa160ce3b0107bfd2106dd45d1
manohar <manumanohar1527@gmail.com>
2020-07-10 06:37:50 +0530

    creating blue top bar

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@ require_once("config/config.php");
 
 if(isset($_SESSION["username"])){
     $userLoggedIn = $_SESSION["username"];
-    echo "welcome ". $userLoggedIn;
+ 
 }
 else{
     header("Location: register.php");
@@ -18,5 +18,18 @@ else{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CONNECTOR</title>
+    <!-- css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src ="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+    <div class="top_bar">
+        <div class="logo">
+           <a href="index.php">Connector</a>
+        </div>
+    </div>
