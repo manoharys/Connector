9fc688365a16938ff81aa5f3b6bf8680023523cc
manohar <manumanohar1527@gmail.com>
2020-07-23 07:55:34 +0530

    updated timeframe

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -77,6 +77,51 @@
                     $time_message = $interval->y . "years ago"; //1+ year ago
                 }
             }
+            else if($interval->m >= 1){
+                if($interval->d == 0){
+                    $days = " ago";
+                }
+                else if($interval->d == 1){
+                    $days = $interval->d . " day ago";
+                }else{
+                    $days = $interval->d . " days ago";
+                }
+
+                if($interval->m == 1){
+                    $time_message = $interval->m ." month". $days;
+                }else{
+                    $time_message = $interval->m . " month". $days;
+                }
+            }
+            else if($interval->d >= 1){
+                if($interval->d == 1){
+                    $time_message =  "yesterday";
+                }
+                else{
+                    $time_message = $interval->d . " days ago";
+                }
+            }
+            else if($interval->h >= 1){
+                  if($interval->h == 1){
+                      $time_message = $interval->h . " hour ago";
+                  }else{
+                    $time_message = $interval->h . " hours ago";
+                }            
+          }
+            else if($interval->i >= 1){
+                if($interval->i == 1){
+                    $time_message = $interval->i . " minute ago";
+                }else{
+                    $time_message = $interval->i . " minutes ago";
+                }
+            }
+            else{
+                if($interval->s <= 30){
+                    $time_message = " Just now";
+                }else{
+                    $time_message = $interval->s . " seconds ago";
+                }
+            }
          }
      } 
    
