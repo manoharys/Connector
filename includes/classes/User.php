67df8d051c610b1ce4b7e32b9c41009ca200a6bc
manohar <manumanohar1527@gmail.com>
2020-07-28 21:53:19 +0530

    isfriend function

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -32,6 +32,16 @@
 			return true;
 		else 
 			return false;
-	}
+    }
+   
+    public function isFriend($username_to_check){
+      $usernameComma = "," . $username_to_check . ".";
+      if(strstr($this->user['friend_array'], $usernameComma) || $username_to_check == $this->user['username']){
+        return true;
+      }else{
+        return false;
+      }
+    }
+
   }
 ?>
