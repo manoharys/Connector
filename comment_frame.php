3d1217609271d802f88b363945459a1c68205714
manohar <manumanohar1527@gmail.com>
2020-07-29 19:45:47 +0530

    posting comments

---
+++ comment_frame.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+	<?php  
+	require 'config/config.php';
+	include("includes/classes/User.php");
+	include("includes/classes/Post.php");
+
+	if (isset($_SESSION['username'])) {
+		$userLoggedIn = $_SESSION['username'];
+		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+		$user = mysqli_fetch_array($user_details_query);
+	}
+	else {
+		header("Location: register.php");
+	}
+
+	?>
+	<script>
+		function toggle() {
+			var element = document.getElementById("comment_section");
+
+			if(element.style.display == "block") 
+				element.style.display = "none";
+			else 
+				element.style.display = "block";
+		}
+	</script>
+
+	<?php  
+	//Get id of post
+	if(isset($_GET['post_id'])) {
+		$post_id = $_GET['post_id'];
+	}
+
+	$user_query = mysqli_query($con, "SELECT added_by, user_to FROM posts WHERE id='$post_id'");
+	$row = mysqli_fetch_array($user_query);
+
+	$posted_to = $row['added_by'];
+
+ 
+	?>
+
+</body>
+</html>
