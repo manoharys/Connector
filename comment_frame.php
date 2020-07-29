2753ac564f68b7f6da82f26b3caf335b73beda16
manohar <manumanohar1527@gmail.com>
2020-07-29 19:51:43 +0530

    inserting comments

--- comment_frame.php
+++ comment_frame.php
@@ -42,8 +42,23 @@
 
 	$posted_to = $row['added_by'];
 
- 
+	if(isset($_POST['postComment' . $post_id])) {
+		$post_body = $_POST['post_body'];
+		$post_body = mysqli_escape_string($con, $post_body);
+		$date_time_now = date("Y-m-d H:i:s");
+		$insert_post = mysqli_query($con, "INSERT INTO comments VALUES ('', '$post_body', '$userLoggedIn', '$posted_to', '$date_time_now', 'no', '$post_id'");
+		echo "<p>Comment Posted! </p>";
+	}
 	?>
+	<form action="comment_frame.php?post_id=<?php echo $post_id; ?>" id="comment_form" name="postComment<?php echo $post_id; ?>" method="POST">
+		<textarea name="post_body"></textarea>
+		<input type="submit" name="postComment<?php echo $post_id; ?>" value="Post">
+	</form>
+
+	<!-- Load comments -->
+
+
+
 
 </body>
 </html>
