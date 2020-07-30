6e52d8e996ee2879a10479925f0e369e8668b135
manohar <manumanohar1527@gmail.com>
2020-07-30 10:34:39 +0530

    small fix - Cannot modify header info

--- comment_frame.php
+++ comment_frame.php
@@ -1,3 +1,19 @@
+<?php  
+	require 'config/config.php';
+	include("includes/classes/User.php");
+	include("includes/classes/Post.php");
+
+	if (isset($_SESSION['username'])) {
+		$userLoggedIn = $_SESSION['username'];
+		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+		$user = mysqli_fetch_array($user_details_query);
+	}
+	else {
+		header("Location: register.php");
+	}
+
+	?>
+
 <html>
 <head>
 	<title></title>
@@ -18,21 +34,7 @@
 
 	</style>
 
-	<?php  
-	require 'config/config.php';
-	include("includes/classes/User.php");
-	include("includes/classes/Post.php");
-
-	if (isset($_SESSION['username'])) {
-		$userLoggedIn = $_SESSION['username'];
-		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
-		$user = mysqli_fetch_array($user_details_query);
-	}
-	else {
-		header("Location: register.php");
-	}
-
-	?>
+	
 	<script>
 		function toggle() {
 			var element = document.getElementById("comment_section");
