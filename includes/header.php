4ecfc88b1cb68922405cd4583ac2c0846e999f4d
manohar <manumanohar1527@gmail.com>
2020-07-31 23:28:21 +0530

    main javascript file

--- includes/header.php
+++ includes/header.php
@@ -34,6 +34,7 @@ else{
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/demo.js"></script>
 </head>
 
 <body>
