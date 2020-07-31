a9e2b8d2cba0f5eef69ee92e60e0a563d5cd3da7
manohar <manumanohar1527@gmail.com>
2020-07-31 23:23:25 +0530

    showing the popup post form

--- profile.php
+++ profile.php
@@ -74,12 +74,46 @@
          }
         ?>
       </form>
+      <input type="submit" class="deep_blue" data-toggle="modal" data-target="#post_form" value="Post Something">
 
     </div>
    
     <div class="main_column column">
       <?php echo $username; ?>
     </div>
+
+    <!-- Modal -->
+<div class="modal fade" id="post_form" tabindex="-1" role="dialog" aria-labelledby="postModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+      <h4 class="modal-title" id="postModalLabel">Post something!</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      
+      </div>
+
+      <div class="modal-body">
+      	<p>This will appear on the user's profile page and also their newsfeed for your friends to see!</p>
+
+      	<form class="profile_post" action="" method="POST">
+      		<div class="form-group">
+      			<textarea class="form-control" name="post_body"></textarea>
+      			<input type="hidden" name="user_from" value="<?php echo $userLoggedIn; ?>">
+      			<input type="hidden" name="user_to" value="<?php echo $username; ?>">
+      		</div>
+      	</form>
+      </div>
+
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" name="post_button" id="submit_profile_post">Post</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
   </div>
