bce5afde97fd7e098de885a509445d20bf3b0d02
manohar <manumanohar1527@gmail.com>
2020-07-31 05:40:39 +0530

    Creating left profile bar

--- profile.php
+++ profile.php
@@ -2,11 +2,28 @@
    include("includes/header.php");
    $profile_pic =  $user['profile_pic'];
 
+   if(isset($_GET['profile_username'])){
+       $username = $_GET['profile_username'];
+       $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username = '$username'");
+       $user_array =  mysqli_fetch_array($user_details_query);
+       $num_of_friends = substr_count($user_array['friend_array'], ',') - 1;
+       $profile_pic = $user_array['profile_pic'];
+   }
+
 ?>
- 
+    <div class="profile_left">
+      <img src="<?php echo $profile_pic ?>" alt="profile pic">
+      
+      <div class="profile_info">
+           <p><?php echo "Posts: ". $user_array['num_posts'];?></p>
+           <p><?php echo "Posts: ". $user_array['num_likes'];?></p>
+           <p><?php echo "Posts: ". $num_of_friends?></p>
+      </div>
+
+    </div>
    
     <div class="main_column column">
-       this is profile page
+      <?php echo $username; ?>
     </div>
 
 
