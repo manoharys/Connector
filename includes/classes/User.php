9f82862c9d72f5dd804469295f225b3bd9871921
manohar <manumanohar1527@gmail.com>
2020-08-01 19:55:22 +0530

    Mutual friend calculation

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -89,5 +89,29 @@
       $query = mysqli_query($this->con, "INSERT INTO friend_requests VALUES('', '$user_to', '$user_from')");
     }
 
+    public function getMutualFriends($user_to_check){
+      $mutualFriends = 0;
+      $user_array = $this->user['friend_array'];
+      $user_array_explode = explode(",", $user_array);
+
+      $query = mysqli_query($this->con, "SELECT friend_array FROM users WHERE username = '$user_to_check'");
+      $row = mysqli_fetch_array($query);
+      $user_to_check_array = $row['friend_array'];
+      $user_to_check_array_explode = explode(",", $user_to_check_array);
+
+      foreach($user_array_explode as $i){
+        
+        foreach($user_to_check_array_explode as $j){
+             
+            if($i == $j && $j != ""){
+              $mutualFriends++;
+            }
+
+        }
+
+      }
+      return $mutualFriends;
+    }
+
   }
 ?>
