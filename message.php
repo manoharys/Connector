4ed6bc834d277ba443e055e3ad8a6bf977b2485d
manohar <manumanohar1527@gmail.com>
2020-08-02 11:49:44 +0530

    Message form setup

--- message.php
+++ message.php
@@ -45,4 +45,22 @@
           if($user_to != "new")
              echo "<h4> You and <a href= '$user_to'>" . $user_to_obj->getFirstAndLastName() . "</a></h4><hr><br>";
         ?>
+
+        <div class="loaded_messages">
+           <form action="" method='POST'>
+              <?php 
+                if($user_to == 'new'){
+                    echo "Select the friend you would like to message <br><br>";
+                    echo "To: <input type='text'>";
+                    echo "<div class='results'></div>";
+                }else{
+                    echo "<textarea name='message_body' id='message_textarea' placeholder='Writer you message....'></textarea>";
+                    echo "<input type='submit' name='post_message' class='info' id='message_submit' value='SEND'>";
+
+                }
+              ?>
+           </form>
+        </div>
+
+
 	</div>
