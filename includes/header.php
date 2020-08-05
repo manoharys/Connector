294bdae717066d0aaf53ebffe13fcfe8d0843bfc
manohar <manumanohar1527@gmail.com>
2020-08-05 10:03:20 +0530

    Adding Message system

--- includes/header.php
+++ includes/header.php
@@ -33,8 +33,8 @@ else{
  
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
         integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/jquery.Jcrop.css" type="text/css">
 	
--- profile.php
+++ profile.php
@@ -1,96 +1,110 @@
 <?php 
-   include("includes/header.php");
+include("includes/header.php");
+
+$message_obj = new Message($con, $userLoggedIn);
+
+if(isset($_GET['profile_username'])) {
+	$username = $_GET['profile_username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
+	$user_array = mysqli_fetch_array($user_details_query);
+
+	$num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
+}
 
 
-   $profile_pic =  $user['profile_pic'];
 
-   if(isset($_GET['profile_username'])){
-       $username = $_GET['profile_username'];
-       $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username = '$username'");
-       $user_array =  mysqli_fetch_array($user_details_query);
-       $num_of_friends = substr_count($user_array['friend_array'], ',');
-       $profile_pic = $user_array['profile_pic'];
-   }
+if(isset($_POST['remove_friend'])) {
+	$user = new User($con, $userLoggedIn);
+	$user->removeFriend($username);
+}
 
-   if(isset($_POST['remove_friend'])){
-      $user = new User($con, $userLoggedIn);
-      $user->removeFriend($username);
-   }
-   
-   if(isset($_POST['add_friend'])){
-      $user = new User($con, $userLoggedIn);
-      $user->sendRequest($username);
-   }
-   
-   if(isset($_POST['respond_friend'])){
-      header("Location: requests.php");
-   }
+if(isset($_POST['add_friend'])) {
+	$user = new User($con, $userLoggedIn);
+	$user->sendRequest($username);
+}
+if(isset($_POST['respond_request'])) {
+	header("Location: requests.php");
+}
 
-?>
-<style type="text/css">
-  .wrapper {
-    margin-left: 0px;
-    padding-left: 0px;
+if(isset($_POST['post_message'])) {
+  if(isset($_POST['message_body'])) {
+    $body = mysqli_real_escape_string($con, $_POST['message_body']);
+    $date = date("Y-m-d H:i:s");
+    $message_obj->sendMessage($username, $body, $date);
   }
 
-  .profile_left {
-    height: 100vh;
-  }
-</style>
+  $link = '#profileTabs a[href="#messages_div"]';
+  echo "<script> 
+          $(function() {
+              $('" . $link ."').tab('show');
+          });
+        </script>";
 
 
-<div class="profile_left">
-  <img src="<?php echo $profile_pic ?>" alt="profile pic">
-  <div class="profile_name"><?php echo $user_array['username'];?></div>
-  <div class="profile_info">
-    <p><?php echo "Posts: ". $user_array['num_posts'];?></p>
-    <p><?php echo "Likes: ". $user_array['num_likes'];?></p>
-    <p><?php echo "Friends: ". $num_of_friends?></p>
-  </div>
+}
 
-  <form action="<?php echo $username; ?>" method="POST">
-    <?php 
-         $profile_user_obj = new User($con, $username);
-         if($profile_user_obj->isClosed()){
-            header("Location: user_closed.php");
-         }
+ ?>
 
-         $logged_in_user_obj =  new User($con, $userLoggedIn);
-         
-         //Checking wheather user is on own profile or friends profile
-         //If is user is on friends profile display Add friend/unfriend btn
-         if($userLoggedIn != $username){
-           if($logged_in_user_obj->isFriend($username)){
-              echo '<input type="submit"  name="remove_friend" class="danger" value="Remove Friend">';
-           }
-           else if($logged_in_user_obj->didReceiveRequest($username)){
-              echo '<input type="submit"  name="respond_friend" class="warning" value="Respond Friend">';
-           }
-           else if($logged_in_user_obj->didSendRequest($username)){
-            echo '<input type="submit"  name="" class="default" value="Request Send">';
-           }
-           else{
-              echo '<input type="submit"  name="add_friend" class="success" value="Add Friend">';
-           }
-         }
-        ?>
-  </form>
-  <input type="submit" class="deep_blue" data-toggle="modal" data-target="#post_form" value="Post Something">
+ 	<style type="text/css">
+	 	.wrapper {
+	 		margin-left: 0px;
+			padding-left: 0px;
+	 	}
 
-  <?php 
-         if($userLoggedIn != $username){
-           echo '<div class="profile_info_button">';
-           echo $logged_in_user_obj->getMutualFriends($username) . " mutual friends";
-           echo '</div>';
-         }
-       ?>
+ 	</style>
+	
+ 	<div class="profile_left">
+ 		<img src="<?php echo $user_array['profile_pic']; ?>">
 
-</div>
+ 		<div class="profile_info">
+ 			<p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
+ 			<p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
+ 			<p><?php echo "Friends: " . $num_friends ?></p>
+ 		</div>
+
+ 		<form action="<?php echo $username; ?>" method="POST">
+ 			<?php 
+ 			$profile_user_obj = new User($con, $username); 
+ 			if($profile_user_obj->isClosed()) {
+ 				header("Location: user_closed.php");
+ 			}
+
+ 			$logged_in_user_obj = new User($con, $userLoggedIn); 
+
+ 			if($userLoggedIn != $username) {
+
+ 				if($logged_in_user_obj->isFriend($username)) {
+ 					echo '<input type="submit" name="remove_friend" class="danger" value="Remove Friend"><br>';
+ 				}
+ 				else if ($logged_in_user_obj->didReceiveRequest($username)) {
+ 					echo '<input type="submit" name="respond_request" class="warning" value="Respond to Request"><br>';
+ 				}
+ 				else if ($logged_in_user_obj->didSendRequest($username)) {
+ 					echo '<input type="submit" name="" class="default" value="Request Sent"><br>';
+ 				}
+ 				else 
+ 					echo '<input type="submit" name="add_friend" class="success" value="Add Friend"><br>';
+
+ 			}
+
+ 			?>
+ 		</form>
+ 		<input type="submit" class="deep_blue" data-toggle="modal" data-target="#post_form" value="Post Something">
+
+    <?php  
+    if($userLoggedIn != $username) {
+      echo '<div class="profile_info_button">';
+        echo $logged_in_user_obj->getMutualFriends($username) . " Mutual friends";
+      echo '</div>';
+    }
 
 
+    ?>
+
+ 	</div>
 
 
-<div class="profile_main_column column">
+	<div class="profile_main_column column">
 
     <ul class="nav nav-tabs" role="tablist" id="profileTabs">
       <li role="presentation" class="active"><a href="#newsfeed_div" aria-controls="newsfeed_div" role="tab" data-toggle="tab">Newsfeed</a></li>
@@ -105,13 +119,36 @@
       </div>
 
 
-      <div role="tabpanel" class="tab-pane fade" id="messages_div">
+      <div role="tabpanel" class="tab-pane fade in" id="messages_div">
+        <?php  
+          echo "<h4>You and <a href='" . $username ."'>" . $profile_user_obj->getFirstAndLastName() . "</a></h4><hr><br>";
 
+          echo "<div class='loaded_messages' id='scroll_messages' style='min-height:350px;max-height:350px;'>";
+            echo $message_obj->getMessages($username);
+          echo "</div>";
+        ?>
+
+
+
+        <div class="message_post">
+          <form action="" method="POST">
+              <textarea name='message_body' id='message_textarea' placeholder='Write your message ...'></textarea>
+              <input type='submit' name='post_message' class='info' id='message_submit' value='Send'>
+          </form>
+
+        </div>
+
+        <script>
+          var div = document.getElementById("scroll_messages");
+          div.scrollTop = div.scrollHeight;
+        </script>
       </div>
 
 
-	</div>
+    </div>
 
+
+	</div>
 
 <!-- Modal -->
 <div class="modal fade" id="post_form" tabindex="-1" role="dialog" aria-labelledby="postModalLabel" aria-hidden="true">
@@ -119,22 +156,20 @@
     <div class="modal-content">
 
       <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <h4 class="modal-title" id="postModalLabel">Post something!</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-            aria-hidden="true">&times;</span></button>
-
       </div>
 
       <div class="modal-body">
-        <p>This will appear on the user's profile page and also their newsfeed for your friends to see!</p>
+      	<p>This will appear on the user's profile page and also their newsfeed for your friends to see!</p>
 
-        <form class="profile_post" action="" method="POST">
-          <div class="form-group">
-            <textarea class="form-control" name="post_body"></textarea>
-            <input type="hidden" name="user_from" value="<?php echo $userLoggedIn; ?>">
-            <input type="hidden" name="user_to" value="<?php echo $username; ?>">
-          </div>
-        </form>
+      	<form class="profile_post" action="" method="POST">
+      		<div class="form-group">
+      			<textarea class="form-control" name="post_body"></textarea>
+      			<input type="hidden" name="user_from" value="<?php echo $userLoggedIn; ?>">
+      			<input type="hidden" name="user_to" value="<?php echo $username; ?>">
+      		</div>
+      	</form>
       </div>
 
 
@@ -147,12 +182,11 @@
 </div>
 
 
-
 <script>
   var userLoggedIn = '<?php echo $userLoggedIn; ?>';
   var profileUsername = '<?php echo $username; ?>';
 
-  $(document).ready(function () {
+  $(document).ready(function() {
 
     $('#loading').show();
 
@@ -161,31 +195,30 @@
       url: "includes/handlers/ajax_load_profile_posts.php",
       type: "POST",
       data: "page=1&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
-      cache: false,
+      cache:false,
 
-      success: function (data) {
+      success: function(data) {
         $('#loading').hide();
         $('.posts_area').html(data);
       }
     });
 
-    $(window).scroll(function () {
+    $(window).scroll(function() {
       var height = $('.posts_area').height(); //Div containing posts
       var scroll_top = $(this).scrollTop();
       var page = $('.posts_area').find('.nextPage').val();
       var noMorePosts = $('.posts_area').find('.noMorePosts').val();
 
-      if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts ==
-        'false') {
+      if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
         $('#loading').show();
 
         var ajaxReq = $.ajax({
           url: "includes/handlers/ajax_load_profile_posts.php",
           type: "POST",
           data: "page=" + page + "&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
-          cache: false,
+          cache:false,
 
-          success: function (response) {
+          success: function(response) {
             $('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
             $('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
 
@@ -202,11 +235,13 @@
 
 
   });
-</script>
+
+  </script>
 
 
 
-</div>
+
+
+	</div>
 </body>
-
 </html>
