f840de5b004a82e04671976744cabab32ce527d7
manohar <manumanohar1527@gmail.com>
2020-08-07 21:49:03 +0530

    insert notification function

--- includes/classes/Notification.php
+++ includes/classes/Notification.php
@@ -14,6 +14,37 @@ class Notification {
 		$query = mysqli_query($this->con, "SELECT * FROM notification WHERE viewed='no' AND user_to='$userLoggedIn'");
 		return mysqli_num_rows($query);
 	}
+
+    public function insertNotification($post_id, $user_to, $type){
+
+		$userLoggedIn = $this->user_obj->getUsername();
+		$userLoggedInName = $this->user_obj->getFirstAndLastName();
+
+		$date_time = date("Y-m-d H:i:s");
+
+		switch($type){
+			case 'comment':
+				 $message = $userLoggedInName . " commented on your post";
+			     break;
+            case 'like':
+			     $message = $userLoggedInName . " liked your post";
+			     break;
+			case 'comment':
+				$message = $userLoggedInName . " posted on your profile";
+				break;
+			case 'comment_non_owner':
+				$message = $userLoggedInName . " commented on a post you commented on";
+				break;		
+			case 'profile_comment':
+				$message = $userLoggedInName . " commented on your profile post";
+			    break;
+		}
+
+		$link = "post.php?id=" . $post_id;
+
+		$insert_query = mysqli_query($this->con, "INSERT INTO notifications VALUES('', '$user_to', '$userLoggedIn', '$message', '$link', '$date_time', 'no', 'no')");
+	}
+
 }
 
  ?>   
