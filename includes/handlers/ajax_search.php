8ab2b0ec2b8412cd54320b52cc0c12c6dd749265
manohar <manumanohar1527@gmail.com>
2020-08-09 22:14:43 +0530

    Configuring the Return String of live search

--- includes/handlers/ajax_search.php
+++ includes/handlers/ajax_search.php
@@ -19,4 +19,34 @@
   else{
     $usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' OR last_name LIKE '$names[0]%') AND user_closed='no' LIMIT 8");
   }
+
+  
+  if($query != ""){
+
+	while($row = mysqli_fetch_array($usersReturnedQuery)) {
+		$user = new User($con, $userLoggedIn);
+
+		if($row['username'] != $userLoggedIn)
+			$mutual_friends = $user->getMutualFriends($row['username']) . " friends in common";
+		else 
+			$mutual_friends == "";
+
+		echo "<div class='resultDisplay'>
+				<a href='" . $row['username'] . "' style='color: #1485BD'>
+					<div class='liveSearchProfilePic'>
+						<img src='" . $row['profile_pic'] ."'>
+					</div>
+
+					<div class='liveSearchText'>
+						" . $row['first_name'] . " " . $row['last_name'] . "
+						<p>" . $row['username'] ."</p>
+						<p id='grey'>" . $mutual_friends ."</p>
+					</div>
+				</a>
+				</div>";
+
+	}
+
+}
+
 ?>
