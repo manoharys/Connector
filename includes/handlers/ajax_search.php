cf48c37defdcf6fa14bf98bf7185fc32df65d945
manohar <manumanohar1527@gmail.com>
2020-08-09 21:57:19 +0530

    Creating live search ajax page

---
+++ includes/handlers/ajax_search.php
@@ -0,0 +1,9 @@
+<?php 
+  include("../../config/config.php");
+  include("../../includes/classes/User.php");
+
+  $query = $_POST['query'];
+  $userLoggedIn = $_POST['userLoggedIn'];
+
+  $names = explode(" ", $query);
+?>
