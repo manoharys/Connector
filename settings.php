aa28900d422c2806ff9786038e45ad76959b28a7
manohar <manumanohar1527@gmail.com>
2020-08-11 06:06:31 +0530

    Creating user Details forms

--- settings.php
+++ settings.php
@@ -8,6 +8,25 @@
       echo "<img src='" .$user['profile_pic'] . "' id='small_profile_pics'>";
     ?>
     <br>
-    <a href="upload.php">Upload new profile picture</a><br><br>
+    <a href="upload.php">Upload new profile picture</a><br><br><br>
 
+    <h4>Modify the values and click 'Update Details'</h5>
+
+    <form action="settings.php" method="POST">
+       First Name: <input type="text" name="first_name" value="<?php echo $user['first_name'];?>"><br>
+       First Name: <input type="text" name="last_name" value="<?php echo $user['last_name'];?>"><br>
+       First Name: <input type="text" name="email" value="<?php echo $user['email'];?>"><br>
+    </form>
+    
+    <h4>Change Password</h4>
+    <form action="settings.php" method="POST">
+      Old password: <input type="password" name="old_password" ><br>
+      New password: <input type="password" name="new_password"><br>
+      New password again: <input type="password" name="new_password_2"><br>
+    </form>
+
+     <h4>Close Account</h4>
+     <form action="setting.php">
+         <input type="submit" name="close_account" id="close_account" value="Close Account">
+     </form>
 </div>
