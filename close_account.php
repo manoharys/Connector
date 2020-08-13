f62cbf5ad8f9278f5ce1e3178bc7364a97e64394
manohar <manumanohar1527@gmail.com>
2020-08-13 19:13:53 +0530

    Close account functionality

--- close_account.php
+++ close_account.php
@@ -1,5 +1,15 @@
 <?php 
   include("includes/header.php");
+
+  if(isset($_POST["cancel"])){
+      header("Location: setting.php");
+  }
+
+  if(isset($_POST['close_account'])){
+      $close_query = mysqli_query($con, "UPDATE users SET user_closed = 'yes' where username='$userLoggedIn'");
+      session_destroy();
+      header("Location: register.php");
+  }
 ?>
 
 <div class="main_column column">
