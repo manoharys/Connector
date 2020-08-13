a808770a8d859b810286f28091f56af693922899
manohar <manumanohar1527@gmail.com>
2020-08-13 22:28:58 +0530

    Outputting the Trending words

--- index.php
+++ index.php
@@ -6,50 +6,82 @@ if(isset($_POST['post'])){
 	$post = new Post($con, $userLoggedIn);
 	$post->submitPost($_POST['post_text'], 'none');
 	
-	?> 
-      <form method="post" id="resetPost"></form>
-      <script>$("#resetPost").submit()</script>
-   <?php
+	?>
+<form method="post" id="resetPost"></form>
+<script>
+	$("#resetPost").submit()
+</script>
+<?php
 }
  ?>
-	<div class="user_details column">
-		<a href="<?php echo $userLoggedIn; ?>">  <img src="<?php echo $user['profile_pic']; ?>"> </a>
+<div class="user_details column">
+	<a href="<?php echo $userLoggedIn; ?>"> <img src="<?php echo $user['profile_pic']; ?>"> </a>
 
-		<div class="user_details_left_right">
-			<a href="<?php echo $userLoggedIn; ?>">
+	<div class="user_details_left_right">
+		<a href="<?php echo $userLoggedIn; ?>">
 			<?php 
 			echo $user['first_name'] . " " . $user['last_name'];
                 
 			 ?>
-			</a>
-			<br>
-			<?php echo "Posts: " . $user['num_posts']. "<br>"; 
+		</a>
+		<br>
+		<?php echo "Posts: " . $user['num_posts']. "<br>"; 
 			echo "Likes: " . $user['num_likes'];
 
 			?>
 
-		</div>
-
 	</div>
 
-	<div class="main_column column">
-		<form class="post_form" action="index.php" method="POST">
-			<textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
-			<input type="submit" name="post" id="post_button" value="Post">
-			<hr>
+</div>
 
-		</form>
+<div class="main_column column">
+	<form class="post_form" action="index.php" method="POST">
+		<textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
+		<input type="submit" name="post" id="post_button" value="Post">
+		<hr>
 
-		<div class="posts_area"></div>
-		<img id="loading" src="assets/images/icons/loading.gif">
+	</form>
 
-         
+	<div class="posts_area"></div>
+	<img id="loading" src="assets/images/icons/loading.gif">
+
+
+</div>
+
+
+<div class="user_details column trends_column">
+
+	<h4 style="margin-left: 25px; color: #4e9adc;">Popular</h4><hr>
+
+	<div class="trends">
+		<?php 
+			$query = mysqli_query($con, "SELECT * FROM trends ORDER BY hits DESC LIMIT 9");
+
+			foreach ($query as $row) {
+				
+				$word = $row['title'];
+				$word_dot = strlen($word) >= 14 ? "..." : "";
+
+				$trimmed_word = str_split($word, 14);
+				$trimmed_word = $trimmed_word[0];
+
+				echo "<div style'padding: 1px'>";
+				echo $trimmed_word . $word_dot;
+				echo "<br></div><br>";
+
+
+			}
+
+	?>
 	</div>
+	
+</div>
 
-	<script>
+
+<script>
 	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
 
-	$(document).ready(function() {
+	$(document).ready(function () {
 
 		$('#loading').show();
 
@@ -58,32 +90,35 @@ if(isset($_POST['post'])){
 			url: "includes/handlers/ajax_load_posts.php",
 			type: "POST",
 			data: "page=1&userLoggedIn=" + userLoggedIn,
-			cache:false,
+			cache: false,
 
-			success: function(data) {
+			success: function (data) {
 				$('#loading').hide();
 				$('.posts_area').html(data);
 			}
 		});
 
-		$(window).scroll(function() {
+		$(window).scroll(function () {
 			var height = $('.posts_area').height(); //Div containing posts
 			var scroll_top = $(this).scrollTop();
 			var page = $('.posts_area').find('.nextPage').val();
 			var noMorePosts = $('.posts_area').find('.noMorePosts').val();
 
-			if ((document.body.scrollHeight == scroll_top + window.innerHeight) && noMorePosts == 'false') {
+			if ((document.body.scrollHeight == scroll_top + window.innerHeight) && noMorePosts ==
+				'false') {
 				$('#loading').show();
 
 				var ajaxReq = $.ajax({
 					url: "includes/handlers/ajax_load_posts.php",
 					type: "POST",
 					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-					cache:false,
+					cache: false,
 
-					success: function(response) {
-						$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
-						$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+					success: function (response) {
+						$('.posts_area').find('.nextPage')
+					.remove(); //Removes current .nextpage 
+						$('.posts_area').find('.noMorePosts')
+					.remove(); //Removes current .nextpage 
 
 						$('#loading').hide();
 						$('.posts_area').append(response);
@@ -98,9 +133,7 @@ if(isset($_POST['post'])){
 
 
 	});
-
-	</script>
-
+</script>
 
 
 
@@ -108,6 +141,8 @@ if(isset($_POST['post'])){
 
 
 
-	</div>
+
+</div>
 </body>
+
 </html>
