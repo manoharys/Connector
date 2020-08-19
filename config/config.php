8e9038676f9d6eec747883dd7df4474c54bcd5a1
manoharys <manumanohar1527@gmail.com>
2020-08-19 23:35:13 +0530

    added remote mysql connection details

--- config/config.php
+++ config/config.php
@@ -2,7 +2,20 @@
   ob_start();
   session_start();
 
-  $con = mysqli_connect("localhost", "root", "", "connector");
+  /* Development Connection */
+     /* $host = "localhost";
+      $db = "connector";
+      $user = "root";
+      $pass = ""; */
+
+  /// Remote Database connection
+    $host = "remotemysql.com";
+    $db = "SdCYQAqHs3";
+    $user = "SdCYQAqHs3";
+    $pass = "LtYgj2HlLF";
+
+
+  $con = mysqli_connect($host, $user, $pass, $db);
 
   if(mysqli_connect_errno()){
     echo "Failed to connect: ". mysqli_connect_errno();
